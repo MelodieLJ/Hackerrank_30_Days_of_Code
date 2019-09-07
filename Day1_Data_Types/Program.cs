dc313d430426aa1ceb6c89f3442073bb002dcf4c
Melodie Joseph <josmelodie@gmail.com>
2019-09-07 23:06:28 +1200

    Day 1: Dealing with data types in C#

---
+++ Day1_Data_Types/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            // Declare second integer, double, and String variables.
+            // Read and save an integer, double, and String to your variables.
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+            double secondDouble = Convert.ToDouble(Console.ReadLine());
+            string secondString = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + secondInt);
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine("{0:0.0}", d + secondDouble);
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + secondString);
+        }
+    }
+}
--- Hackerrank_30_Days_of_Code/Program.cs
+++ Hackerrank_30_Days_of_Code/Program.cs
@@ -10,6 +10,18 @@ namespace Hackerrank_30_Days_of_Code
     {
         static void Main(string[] args)
         {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+            Console.WriteLine(inputString);
         }
     }
 }
