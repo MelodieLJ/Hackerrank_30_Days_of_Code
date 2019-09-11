841f990f2175eabadbfb6b2226249dfefbe577ed
Melodie Joseph <josmelodie@gmail.com>
2019-09-11 22:14:05 +1200

    Basic mathematical operations

--- Day2_Operator/Program.cs
+++ Day2_Operator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Day2_Operator
+namespace Day2_Operators
 {
     //Challenge: Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
 
