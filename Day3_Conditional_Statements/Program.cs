37200cd202d0cf4d7fce601c8b587a28e5e29fe7
Melodie Joseph <josmelodie@gmail.com>
2019-09-12 22:56:44 +1200

    Day03 - Simple if else statement

---
+++ Day3_Conditional_Statements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                if (n >= 2 && n <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (n >= 6 & n <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (n > 20)
+                {
+                    Console.WriteLine("Not weird");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Weird");
+            }
+
+        }
+    }
+}
