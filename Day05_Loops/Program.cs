67f2a21ca5a8c7b6aa7f62d49fa2f467a89b964f
Melodie Joseph <josmelodie@gmail.com>
2019-09-21 21:36:23 +1200

    Day 5: Simple Loop to multiples of a number read from the console

---
+++ Day05_Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05_Loops
+{
+    class Program
+    {
+        //Challenge: Given an integer, n, print its first 10 multiples. Each multiple n x i(where 1 < i < 10) should be printed on a new line in the form: n x i = result.
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                //int result = i * n;
+                Console.WriteLine($"{n} x {i} = {i * n}");
+            }
+        }
+    }
+}
