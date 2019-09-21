a2cd711c60fc462c01de582e0e4e84edc14ceeed
Melodie Joseph <josmelodie@gmail.com>
2019-09-22 00:41:18 +1200

    Day 7: Simple Array Reversal

---
+++ Day07_Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07_Arrays
+{
+    //Challenge: Reverse an Array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] temp = Console.ReadLine().Split(' ');
+
+            int[] arr = Array.ConvertAll(temp, Int32.Parse);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
